865bbce5c0ca161f07cc9fcde3d25a04e6145b98
Анисимов Александр <ujc90@mail.ru>
2022-03-29 17:21:44 +0300

    edit task js

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,23 @@
+let clicker = document.getElementById("clicker__counter");
+let image = document.getElementById("cookie");
+let cookieClass =  image.className;
+let speed =document.getElementById ("clicker__speed");
+clicker.textContent = 0;
+clicker__speed.textContent = 0;
+//console.log(clicker);
+//console.log(image);
+//console.log(cookieClass);
+//console.log(clicker.textContent);
+image.onclick = click;
+function click () {
+    clicker.textContent++;
+    if (clicker.textContent % 2 === 0) {
+      image.width = 200; 
+    } else {
+      image.width = 250;
+   }        
+   let x =  setTimeout(function(){ clicker.textContent;}, 1000)
+   clicker__speed.textContent = 60 / x;
+}
+
+
