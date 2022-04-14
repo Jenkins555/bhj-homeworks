c7f79010da1dcf636bd6a4b747636696764ffe1e
Анисимов Александр <ujc90@mail.ru>
2022-04-14 15:34:16 +0300

    edit task.js

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,13 +2,13 @@ let menuLink = document.getElementsByClassName('menu__link');
 let menuItem = document.getElementsByClassName('menu__item');
 let menuArr = Array.from(menuItem);
 
-console.log(menuItem[1].querySelector('.menu_sub'));
+///console.log(menuItem[1].querySelector('.menu_sub'));
 
 for (let i = 0; i < menuLink.length; i++) {
     menuLink[i].onclick = function () {
-        if (menuItem[i].querySelector('.menu_sub') !== null && menuItem[i].querySelector('.menu_active') === null) {
+        if (menuItem[i].querySelector('.menu_sub') !== null ) {
             menuArr[i].querySelector('.menu_sub').classList.toggle('menu_active');
-            return menuArr[i].href = false;
+            return false;
         }
     }
 }
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,30 +1,30 @@
 let slides = document.getElementsByClassName('slider__item');
-let arrow_next = document.getElementsByClassName('slider__arrow_next');
-let arrow_prev = document.getElementsByClassName('slider__arrow_prev')
+let arrowNext = document.getElementsByClassName('slider__arrow_next');
+let arrowPrev = document.getElementsByClassName('slider__arrow_prev')
+let slidesArr = Array.from(slides);
 
-let i = 0;
+console.log(slidesArr)
+arrowNext[0].onclick = function () {
+  let activeInd = slidesArr.findIndex((element) => element.classList.contains("slider__item_active"));
+  slides[activeInd].classList.remove("slider__item_active");
 
-arrow_prev[0].onclick = function () {
-    i--;
-    if (i >= slides.length) {
-        slides[i + 1].classList.remove("slider__item_active");
-        i = 0;
-        slides[i].classList.add("slider__item_active");
-    } else {
-        slides[i + 1].classList.remove("slider__item_active");
-        slides[i].classList.add("slider__item_active");
-    }
+  (activeInd < 4) ? activeInd++ : activeInd = 0;
+  //console.log(activeInd);
+  slides[activeInd].classList.add("slider__item_active");
 }
 
-arrow_next[0].onclick = function () {
-    i++;
-    if (i >= slides.length) {
-        slides[i - 1].classList.remove("slider__item_active");
-        i = 0;
-        slides[i].classList.add("slider__item_active");
-    } else {
-        slides[i - 1].classList.remove("slider__item_active");
-        slides[i].classList.add("slider__item_active");
-    }
+arrowPrev[0].onclick = function () {
+  console.log('click');
+  let activeInd = slidesArr.findIndex((element) => element.classList.contains("slider__item_active"));
+  slides[activeInd].classList.remove("slider__item_active");
+
+  (activeInd < 1) ? activeInd = 4 : activeInd--;
+  slides[activeInd].classList.add("slider__item_active");
+  //console.log(activeInd);
 
 }
+
+
+
+
+
