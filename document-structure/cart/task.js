6cf4a5bc4b8222c4a936f927cd508facf128dcdd
Анисимов Александр <ujc90@mail.ru>
2022-05-17 09:19:42 +0300

    edit task

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -29,30 +29,24 @@ function addInCard(e) {
     let thisImg = thisPruductCard.querySelector('.product__image').src;///изображение
     let thisValue = thisPruductCard.querySelector('.product__quantity-value').textContent;///количеств товара
     let productId = target.closest('.product').dataset.id;///идентификатор карточки товара
-    let array = Array.from(cartProducts) /// массив товаров в корзине
-    ///
+
     const html = `<div class="cart__product" data-id="` + productId + `">
     <img class="cart__product-image" src="`+ thisImg + `">    
     <div class="cart__product-count">` + thisValue + `</div>
 </div>`
-    ///
-    //console.log(cartCount);
 
-    //console.log(thisValue);
     ///ищем в массиве товаров корзины товар, с тем же id , как у добавляемого товара.
     let arrCheck = Array.from(cartProducts).find(item => item.dataset.id === productId);
-    /// console.log(arrCheck.querySelector('.cart__product-count'));
-    console.log(arrCheck)
-    ///выводим товар arrCheck.querySelector('.cart__product-count').textContent;
-    let number = arrCheck.querySelector('.cart__product-count').textContent;
-    ///если товар с таким id существует, изменяем количество товара
-    if (arrCheck) {
+
+    ///если товар существует, увеличиваем количество позиций
+    if (arrCheck != undefined) {
+        let number = arrCheck.querySelector('.cart__product-count').textContent;
         arrCheck.querySelector('.cart__product-count').textContent = (+number) + (+thisValue);
-      ///  console.log('есть')
-    } else {
-        ///если такой товар отсутствует, добавляем
-        cartProduct[0].insertAdjacentHTML("beforeEnd", html) ///добавляем код
+        return;
     }
+    ///добавляем товар
+    cartProduct[0].insertAdjacentHTML("beforeEnd", html)
+
 }
 
 productAdd.forEach(el => {
